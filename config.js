b5bdcd332e88c1202019c142748f5bf8014cc1cc
Val-Luttole <91841984+233pachino@users.noreply.github.com>
2024-01-09 16:20:04 +0000

    Update 2 files

---
+++ config.js
@@ -0,0 +1,81 @@
+/*
+* https://rentry.org/teralomaniac_clewd
+* https://github.com/teralomaniac/clewd
+*/
+
+// SET YOUR COOKIE BELOW
+
+module.exports = {
+    "Cookie": "",
+    "CookieArray": [
+        "sessionKey=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",
+        "sessionKey=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",
+        "sessionKey=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",
+        "sessionKey=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",
+        "sessionKey=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",
+        "sessionKey=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",
+        "sessionKey=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",
+        "sessionKey=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",
+        "sessionKey=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",
+        "sessionKey=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",
+        "sessionKey=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",
+        "sessionKey=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",
+        "sessionKey=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",
+        "sessionKey=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",
+        "sessionKey=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",
+        "sessionKey=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",
+        "sessionKey=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"
+    ],
+    "Cookiecounter": 1,
+    "CookieIndex": 0,
+    "ProxyPassword": "dvx632pLZyylig4A0S",
+    "Ip": "127.0.0.1",
+    "Port": 8444,
+    "localtunnel": false,
+    "BufferSize": 1,
+    "SystemInterval": 3,
+    "rProxy": "",
+    "api_rProxy": "",
+    "padtxt_placeholder": "",
+    "PromptExperimentFirst": "",
+    "PromptExperimentNext": "",
+    "PersonalityFormat": "{{char}}'s personality: {{personality}}",
+    "ScenarioFormat": "Dialogue scenario: {{scenario}}",
+    "Settings": {
+        "RenewAlways": true,
+        "RetryRegenerate": false,
+        "PromptExperiments": true,
+        "SystemExperiments": true,
+        "PreventImperson": false,
+        "AllSamples": false,
+        "NoSamples": false,
+        "StripAssistant": false,
+        "StripHuman": false,
+        "PassParams": false,
+        "ClearFlags": true,
+        "PreserveChats": false,
+        "LogMessages": true,
+        "FullColon": true,
+        "padtxt": 15000,
+        "xmlPlot": true,
+        "Superfetch": true
+    }
+}
+
+/*
+ BufferSize
+ * How many characters will be buffered before the AI types once
+ * lower = less chance of `PreventImperson` working properly
+
+ ---
+
+ SystemInterval
+ * How many messages until `SystemExperiments alternates`
+
+ ---
+
+ Other settings
+ * https://gitgud.io/ahsk/clewd/#defaults
+ * and
+ * https://gitgud.io/ahsk/clewd/-/blob/master/CHANGELOG.md
+ */
